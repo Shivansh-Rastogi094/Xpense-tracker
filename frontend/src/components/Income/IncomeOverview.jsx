d27e46ba969a664d0b16af7e6de4b9016cb8e449
Shivansh-Rastogi094 <rshivansh094@gmail.com>
2026-01-11 12:04:54 +0530

    started income model and components

---
+++ frontend/src/components/Income/IncomeOverview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {LuPlus} from "react-icons/lu"
+import CustomBarChart from '../Charts/CustomBarChart'
+
+const IncomeOverview = ({transactions,onAddIncome}) => {
+  return (
+    <div>IncomeOverview</div>
+  )
+}
+
+export default IncomeOverview
